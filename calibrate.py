217ba6c3821da3d00d6a8392adcee5a5f7b22d9f
Alfredo Rivero <alfredo.rivero@outlook.com>
2020-09-21 17:38:02 -0700

    Modified calibrate.py into runable script

--- calibrate.py
+++ calibrate.py
@@ -1,6 +1,9 @@
 import cv2
-import glob
+import json
+import sys
 import numpy as np
+from getopt import getopt
+from os import listdir
 
 
 def define_board_points(board_dim):
@@ -17,9 +20,9 @@ def find_calibration_data(img_dir, board_dim):
     img_points = []  # 2D points in image plane of estimated board points
 
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-    images = glob.glob(img_dir)
+    images = listdir(img_dir)
     for file_name in images:
-        img = cv2.imread(file_name)
+        img = cv2.imread(img_dir + "/" + file_name)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         # Find the chess board corners
@@ -64,3 +67,57 @@ def find_mean_square_calibration_error(calib_dataset, calibration):
         total_error += error
 
     return total_error / len(obj_points)
+
+
+def compute_camera_calibration(img_dir, board_dim):
+    calib_dataset = find_calibration_data(img_dir, board_dim)
+    calibration = calibrate_camera(calib_dataset)
+    mean_error = find_mean_square_calibration_error(calib_dataset, calibration)
+
+    return (calibration, mean_error)
+
+
+if __name__ == "__main__":
+    # Use getopt to gather our arguments
+    img_dir = None
+    save_dir = None
+    board_dim_x = None
+    board_dim_y = None
+
+    opts, args = getopt(sys.argv[1:], "d:s:x:y:")
+    for opt, arg in opts:
+        if opt == "-d":
+            img_dir = arg
+        elif opt == "-s":
+            save_dir = arg
+        elif opt == "-x":
+            board_dim_x = int(arg)
+        elif opt == "-y":
+            board_dim_y = int(arg)
+
+    # Communicate the need for missing arguments
+    if img_dir is None:
+        print("Missing calibration image directory (-d)!")
+        exit()
+    if save_dir is None:
+        print("Missing calibration save directory (-s)!")
+        exit()
+    if board_dim_x is None:
+        print("Missing calibration board x dimmension (-x)!")
+        exit()
+    if board_dim_y is None:
+        print("Missing calibration board y dimmension (-y)!")
+        exit()
+
+    # Process and save our camera calibration
+    board_dim = (board_dim_x, board_dim_y)
+    calibration, mean_error = compute_camera_calibration(img_dir, board_dim)
+    _, k_mtx, dist, _, _ = calibration
+
+    export_data = {
+        "k_matrix": k_mtx.tolist(),
+        "dist_coeff": dist.tolist(),
+        "mean_error": mean_error
+    }
+    with open(save_dir, "w") as saved_calibration:
+        json.dump(export_data, saved_calibration)
